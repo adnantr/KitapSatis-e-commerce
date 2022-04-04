a7537dfb30c2ba3c6c0f1ed82f39b94e2f8aafd7
Adnan Tatar <57808681+adnantr@users.noreply.github.com>
2022-04-04 12:54:23 +0300

    Add project files.

---
+++ KitapSatis/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KitapSatis.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
